f81ad08dd6cde2b70a334570f9c88e01bc9b065a
asgardar <asgardar@gmail.com>
2017-02-01 20:18:37 -0600

    Initial version

---
+++ src/main/java/com/awsbuddy/AwsbuddyprojApplication.java
@@ -0,0 +1,12 @@
+package com.awsbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsbuddyprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsbuddyprojApplication.class, args);
+	}
+}
